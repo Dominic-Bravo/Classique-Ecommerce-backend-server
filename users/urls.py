dd690bb3b250216e61730dbf738284955669c4c5
Dominic-Bravo <dominicianbravo017@gmail.com>
2026-04-27 09:10:59 +0800

    feat: added register

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,10 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
 
-from users.views import PSIDLoginView
+from users.views import PSIDLoginView, RegisterView
 
 urlpatterns = [
+    path('register/', RegisterView.as_view()),
     path('login/', TokenObtainPairView.as_view()),
     path('refresh/', TokenRefreshView.as_view()),
     
--- users/views.py
+++ users/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 # apps/users/views.py
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+from .serializers import RegisterSerializer
 from .services import get_or_create_user_from_psid
 from .utils import generate_tokens
+
+
+class RegisterView(CreateAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
+
 
 class PSIDLoginView(APIView):
     def post(self, request):
